51add0faff53d84248205b220cff0efd422e0d9e
achmad lutfi <alhacker1996@gmail.com>
2025-12-31 14:32:36 +0700

    add EventHandler btn click

--- GetStartedApp/Views/MainWindow.axaml.cs
+++ GetStartedApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
 
 namespace GetStartedApp.Views;
 
@@ -7,5 +9,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ButtonOnClick(object? sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine("Click!!!");
     }
 }
